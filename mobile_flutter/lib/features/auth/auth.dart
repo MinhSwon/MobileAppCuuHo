3da7a2c2de1262753cb6fe9391498d86f99aef39
HauTrungThanh <pethanh0410@gmail.com>
2026-07-12 22:54:35 +0700

    add mobile login flow

---
+++ mobile_flutter/lib/features/auth/auth.dart
@@ -0,0 +1,3 @@
+export 'presentation/login_screen.dart';
+export 'presentation/public_access_screen.dart';
+export 'presentation/register_screen.dart';
---
+++ mobile_flutter/lib/features/features.dart
@@ -0,0 +1,6 @@
+export 'admin/admin.dart';
+export 'auth/auth.dart';
+export 'chat/chat.dart';
+export 'citizen/citizen.dart';
+export 'rescue/rescue.dart';
+export 'shell/shell.dart';
